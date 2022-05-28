171165f0a0cdb88f7e240165c8a47fbfdc56a1c0
yash nirmal <yash15nirmal@gmail.com>
2022-05-28 15:19:47 +0530

    initial commmit

--- src/components/Third.js
+++ src/components/Third.js
@@ -6,7 +6,7 @@ export default function Third() {
 
 
   const projectDetails = [
-    ["My portfolio website","A website to show details about my work and myself","#",["ReactJs","pure CSS"]],
+    ["My portfolio website","A website to show details about my work and myself","https://yash-nirmal.netlify.app/",["ReactJs","pure CSS"]],
     ["MemeMatic","This is a website which shows you trending memes which are scrapped from Reddit and then fetched using an API call","https://meme-matic.netlify.app/",["ReactJs","pure CSS","Fetch API","Localstorage"]],
     ["Crypto Tracker and News","This website show the almost all the data related to 200 cryptocurrencies aand shows you news happening around the crypto world","https://crypto-news-and-tracker.netlify.app/",["ReactJs","pure CSS","Fetch API","react-chart-js 2","MaterialUI"]],
     ["Twitter Home Page Clone","This a single page clone of the Twitter's home page ","https://tweeter-home-clone.netlify.app/",["ReactJs","pure CSS"]],
