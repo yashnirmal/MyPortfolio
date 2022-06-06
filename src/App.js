d8aa6bbb72b3487d7adc3e6f5e39ff45c4e176d6
yash nirmal <yash15nirmal@gmail.com>
2022-06-06 13:24:59 +0530

    added download resume btn

--- src/App.js
+++ src/App.js
@@ -5,31 +5,31 @@ import Third from './components/Third';
 import Second from './components/Second';
 import Scroll from './components/Scroll';
 import Fourth from './components/Fourth';
-import Snackbar from "@material-ui/core/Snackbar";
+// import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import Resume from "./components/Resume";
 
 
-function Alert(props) {
-  return <MuiAlert elevation={6} variant="filled" {...props} />;
-}
+// function Alert(props) {
+//   return <MuiAlert elevation={6} variant="filled" {...props} />;
+// }
 
 function App() {
 
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    console.log("Here")
-    setOpen(true);
-    console.log(open);
-  };
+  // const handleClick = () => {
+  //   console.log("Here")
+  //   setOpen(true);
+  //   console.log(open);
+  // };
 
-  const handleClose = (event, reason) => {
-    if (reason === "clickaway") {
-      return;
-    }
-    setOpen(false);
-  };
+  // const handleClose = (event, reason) => {
+  //   if (reason === "clickaway") {
+  //     return;
+  //   }
+  //   setOpen(false);
+  // };
 
 
   return (
